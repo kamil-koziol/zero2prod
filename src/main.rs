fe9766c2c318b6097b706c6c284b480dd8c80aec
Kamil Kozioł <koziol.kamil@protonmail.com>
2023-06-08 16:37:48 +0200

    Finished chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8080")
-.expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
-    run(listener)?.await
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection_pool)?.await
 }
-
